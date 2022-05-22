e8c9d1ae5e9db8bd8c222e86501f34e59b3cb266
inaveentata <inaveentata@outlook.com>
2022-05-23 01:41:14 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    
+    <section>
+      <h2>About us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium deserunt aspernatur voluptas beatae. Beatae hic voluptatem perspiciatis cum est voluptas necessitatibus in adipisci quos, cumque atque, soluta officia, nesciunt ullam doloribus iure nisi doloremque velit deleniti? Ab magni doloremque rem maiores dolore sint suscipit. Ab vel doloremque similique est praesentium.</p>
+    </section>
+  )
+}
+
+export default About
---
+++ src/pages/SingleCocktail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleCocktail = () => {
+  return (
+    <div>SingleCocktailPage</div>
+  )
+}
+
+export default SingleCocktail
