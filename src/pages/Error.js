3716abd77ca29589161053b19d071471ebf4cbcc
inaveentata <inaveentata@outlook.com>
2022-05-23 15:15:37 +0530

    Edited error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <section>
-      <h2>oops! you came to wrong page</h2>
-      <Link to="/">back home</Link>
+    <section className="error-section">
+      <h2>oops! you came to wrong page click below 👇 </h2>
+      <button>
+        <Link to="/" className="btn">back home</Link>
+      </button>
     </section>
   );
 };
